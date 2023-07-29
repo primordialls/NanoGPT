c9bd81459b30d371b1019f087e82039b51340ed5
Elia Doehler <eliadoehler@me.com>
2023-07-29 16:03:28 +0200

    test

--- bigram.py
+++ bigram.py
@@ -82,7 +82,15 @@ class Head(nn.Module):
         out = wei @ v
         return out
     
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self attention in parallel """
 
+    def __init__(self,num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads],dim=-1)
 
     
 class BigramLanguageModel(nn.Module):
